b146005621a49b3935745175511a98c9f5cdd6ea
Sorana-Maria <87468611+Sorana-Maria@users.noreply.github.com>
2023-05-31 13:43:19 +0200

    main call

---
+++ main.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    # get input data
+    lat = input("Enter latitude")
+    lon = input("Enter longitude")
+
+    # create the meteo request with the input data
+    meteo_data = open_meteo(latitude=lat, longitude=lon)
+
+    plot_data(meteo_data)
